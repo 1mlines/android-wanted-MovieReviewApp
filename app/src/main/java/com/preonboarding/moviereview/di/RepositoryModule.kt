f34466a2393a4025e645caefb9e0f3aae9b021a9
sujin-kk <sjoo0002@gmail.com>
2022-10-03 18:12:43 +0900

    [FIX] inject dependency with repository

--- app/src/main/java/com/preonboarding/moviereview/di/RepositoryModule.kt
+++ app/src/main/java/com/preonboarding/moviereview/di/RepositoryModule.kt
@@ -1,4 +1,19 @@
 package com.preonboarding.moviereview.di
 
-object RepositoryModule {
+import com.preonboarding.moviereview.data.remote.repository.RemoteRepositoryImpl
+import com.preonboarding.moviereview.domain.repository.remote.RemoteRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindsRemoteRepository(
+        remoteRepositoryImpl: RemoteRepositoryImpl
+    ): RemoteRepository
 }
