22ee7444619f9a4ca3d77ded445cac6644467455
sujin-kk <sjoo0002@gmail.com>
2022-10-03 17:14:04 +0900

    [CREATE] build project architecture

---
+++ app/src/main/java/com/preonboarding/moviereview/presentation/ui/home/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package com.preonboarding.moviereview.presentation.ui.home
+
+import com.preonboarding.moviereview.presentation.common.base.BaseViewModel
+
+class HomeViewModel: BaseViewModel() {
+}
