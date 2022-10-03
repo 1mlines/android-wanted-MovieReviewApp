3fee3c6e96c9a27e95b9448d9a76f641080e668e
sujin-kk <sjoo0002@gmail.com>
2022-10-03 17:54:18 +0900

    [STYLE] add custom header

--- app/src/main/java/com/preonboarding/moviereview/MovieReviewApplication.kt
+++ app/src/main/java/com/preonboarding/moviereview/MovieReviewApplication.kt
@@ -1,6 +1,7 @@
 package com.preonboarding.moviereview
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -9,5 +10,6 @@ class MovieReviewApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
