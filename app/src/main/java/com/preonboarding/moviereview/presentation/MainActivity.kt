2a888a76874c9b219670f7e1ba659e00188e1a81
gyurim <gyurim1250@gmail.com>
2022-10-04 16:33:05 +0900

    chore: build.gradle 설정

--- app/src/main/java/com/preonboarding/moviereview/presentation/MainActivity.kt
+++ app/src/main/java/com/preonboarding/moviereview/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.preonboarding.moviereview
+package com.preonboarding.moviereview.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.preonboarding.moviereview.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
