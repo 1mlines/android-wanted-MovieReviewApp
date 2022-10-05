a2ed13cdb7d57fe4007575bea7cdc18bfa985242
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-10-05 23:17:14 +0900

    [FEAT] Timber Plant 추가

--- app/src/main/java/com/preonboarding/MovieApplication.kt
+++ app/src/main/java/com/preonboarding/MovieApplication.kt
@@ -2,6 +2,19 @@ package com.preonboarding
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MovieApplication: Application()
+class MovieApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(object : Timber.DebugTree() {
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                super.log(priority, "Debug[$tag]", message, t)
+            }
+        })
+    }
+}
+
