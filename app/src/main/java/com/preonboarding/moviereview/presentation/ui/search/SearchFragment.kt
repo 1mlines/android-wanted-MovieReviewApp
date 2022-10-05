b64c40ee9ee6539602fa4c863c13205350873c41
JaesungLeee <viki0114@naver.com>
2022-10-05 14:52:13 +0900

    [FEAT] Search 화면 네비게이션 적용

---
+++ app/src/main/java/com/preonboarding/moviereview/presentation/ui/search/SearchFragment.kt
@@ -0,0 +1,15 @@
+package com.preonboarding.moviereview.presentation.ui.search
+
+import android.os.Bundle
+import android.view.View
+import com.preonboarding.moviereview.R
+import com.preonboarding.moviereview.databinding.FragmentSearchBinding
+import com.preonboarding.moviereview.presentation.common.base.BaseFragment
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
