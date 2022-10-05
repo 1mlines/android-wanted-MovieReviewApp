8e7419f0a5b9461d187e3b6b1e7aafb278e7ef0d
JaesungLeee <viki0114@naver.com>
2022-10-05 16:10:37 +0900

    [FEAT] CustomSearchView 구현

--- app/src/main/java/com/preonboarding/moviereview/presentation/ui/search/SearchFragment.kt
+++ app/src/main/java/com/preonboarding/moviereview/presentation/ui/search/SearchFragment.kt
@@ -1,7 +1,14 @@
 package com.preonboarding.moviereview.presentation.ui.search
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import com.preonboarding.moviereview.R
 import com.preonboarding.moviereview.databinding.FragmentSearchBinding
 import com.preonboarding.moviereview.presentation.common.base.BaseFragment
@@ -11,5 +18,22 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setMenuProvider()
+
+    }
+
+    // onCreateOptionsMenu Deprecated
+    private fun setMenuProvider() {
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_toolbar_searchview, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                TODO("Not yet implemented")
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
