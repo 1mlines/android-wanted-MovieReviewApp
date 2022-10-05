f9f2f65ce41b9f26f1cb8a16396700091d9536fb
JaesungLeee <viki0114@naver.com>
2022-10-05 18:18:50 +0900

    [FEAT] 검색 UI 구현

--- app/src/main/java/com/preonboarding/moviereview/presentation/ui/search/SearchFragment.kt
+++ app/src/main/java/com/preonboarding/moviereview/presentation/ui/search/SearchFragment.kt
@@ -1,23 +1,25 @@
 package com.preonboarding.moviereview.presentation.ui.search
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
-import androidx.appcompat.widget.SearchView
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
-import androidx.lifecycle.Lifecycle
 import com.preonboarding.moviereview.R
 import com.preonboarding.moviereview.databinding.FragmentSearchBinding
 import com.preonboarding.moviereview.presentation.common.base.BaseFragment
+import com.preonboarding.moviereview.presentation.common.extension.navigateUp
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        bindViews()
+
+    }
+
+    private fun bindViews() {
+        binding.tbSearch.setNavigationOnClickListener {
+            navigateUp()
+        }
     }
 
 }
