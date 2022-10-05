49eedc0cd11f10ceed4a8a8fbdf50ef9aa151f1e
leehandsub <gustjq7027@naver.com>
2022-10-06 01:41:32 +0900

    [FEAT] firebase 해당영화에 리뷰가 없을때 있을때 구분

--- app/src/main/java/com/preonboarding/moviereview/presentation/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/preonboarding/moviereview/presentation/ui/detail/DetailFragment.kt
@@ -3,22 +3,45 @@ package com.preonboarding.moviereview.presentation.ui.detail
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import com.google.firebase.database.*
 import com.preonboarding.moviereview.R
 import com.preonboarding.moviereview.databinding.FragmentDetailBinding
 import com.preonboarding.moviereview.presentation.common.base.BaseFragment
+import com.preonboarding.moviereview.presentation.common.const.FIRE_BASE_URL
 import com.preonboarding.moviereview.presentation.common.extension.navigate
 import com.preonboarding.moviereview.presentation.common.extension.navigateUp
-import com.preonboarding.moviereview.presentation.ui.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+
 
 @AndroidEntryPoint
 class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
     private val detailViewModel : DetailViewModel by viewModels()
-
+    private lateinit var database: DatabaseReference
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initListener()
-        detailViewModel.searchReviewMovieList()
+        database = FirebaseDatabase.getInstance().reference
+        // 무비 id 가져와야한다.
+        val ref = database.database.getReferenceFromUrl("$FIRE_BASE_URL")
+        ref.child("1").addValueEventListener(
+            object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if(dataSnapshot.value==null){
+                        //리뷰가 없을때
+                    }
+                    else{
+                        detailViewModel.searchReviewMovieList()
+                        //리뷰가 있을때
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    println("The read failed: " + databaseError.code)
+                }
+            }
+        )
+
     }
 
     private fun initListener() {
