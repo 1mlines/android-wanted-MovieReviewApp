b0fbeb42c7c000c8f03cd32196a18f38a7e0d8dd
YellowC-137 <55780312+YellowC-137@users.noreply.github.com>
2022-10-07 04:20:10 +0900

    [FEAT] 두번째 화면 #3

---
+++ presentation/src/main/java/com/preonboarding/presentation/view/adapter/DetailReviewAdapter.kt
@@ -0,0 +1,84 @@
+package com.preonboarding.presentation.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.preonboarding.domain.model.Review
+import com.preonboarding.presentation.R
+import com.preonboarding.presentation.databinding.ItemReviewBinding
+
+//리뷰 아이템
+
+
+class DetailReviewAdapter : ListAdapter<Review, DetailReviewAdapter.ReviewViewHolder>(diffUtil) {
+
+    interface DeleteItemClick {
+        fun onClick(view: View, position: Int)
+    }
+
+    interface EditItemClick {
+        fun onClick(view: View, position: Int)
+    }
+    interface NickNameClick{
+        fun onClick(view: View, position: Int)
+    }
+
+    var deleteItemClick: DeleteItemClick? = null
+    var editItemClick: EditItemClick? = null
+    var nickNameClick:NickNameClick? =null
+
+
+    inner class ReviewViewHolder(private val binding: ItemReviewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(items: Review) {
+
+            binding.tvNickName.text = items.nickname
+            binding.tvContent.text = items.content
+            binding.rating.rating = items.rating
+            Glide.with(binding.ivEdit.context).load(items.imageUri).error(R.drawable.no_img)
+                .into(binding.ivReviewImg)
+            binding.tvReviewDate.text = items.date
+
+        }
+    }
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
+        return ReviewViewHolder(
+            ItemReviewBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<Review>() {
+            override fun areItemsTheSame(
+                oldItem: Review,
+                newItem: Review
+            ): Boolean {
+                return oldItem.hashCode() == newItem.hashCode()
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Review,
+                newItem: Review
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+}
