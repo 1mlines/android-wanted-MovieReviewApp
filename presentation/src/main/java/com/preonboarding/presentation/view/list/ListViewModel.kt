1565a8752a2443ec0bc82326cffc6e6fe98fe6b6
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-10-06 23:37:27 +0900

    [FEAT] 영화 API 연동 및 화면 표현 로직 추가

--- presentation/src/main/java/com/preonboarding/presentation/view/list/ListViewModel.kt
+++ presentation/src/main/java/com/preonboarding/presentation/view/list/ListViewModel.kt
@@ -1,11 +1,40 @@
 package com.preonboarding.presentation.view.list
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.preonboarding.domain.model.Movie
+import com.preonboarding.domain.model.Result
+import com.preonboarding.domain.usecase.GetBoxOfficeListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class ListViewModel @Inject constructor(
+    private val getBoxOfficeListUseCase: GetBoxOfficeListUseCase
+) : ViewModel() {
 
-): ViewModel() {
+    private val _movies: MutableLiveData<List<Movie>> = MutableLiveData()
+    val movies: LiveData<List<Movie>> get() = _movies
+
+    fun fetchMovieList() {
+        viewModelScope.launch {
+            when (val result = getBoxOfficeListUseCase.invoke()) {
+                is Result.Success -> {
+                    val list = result.data
+
+                    Timber.w("ViewModel Success!")
+                    Timber.w("$list")
+
+                    _movies.postValue(list)
+                }
+                is Result.Error -> {
+                    Timber.w("Error")
+                }
+            }
+        }
+    }
 }
