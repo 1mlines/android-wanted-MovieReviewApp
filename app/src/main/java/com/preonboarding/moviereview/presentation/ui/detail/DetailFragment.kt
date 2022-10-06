6dbcac53e8e7942edc57128f5f6b2e8efa11937e
leehandsub <gustjq7027@naver.com>
2022-10-06 23:30:46 +0900

    [REFACTOR] home->detail id 임의로 1로지정

--- app/src/main/java/com/preonboarding/moviereview/presentation/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/preonboarding/moviereview/presentation/ui/detail/DetailFragment.kt
@@ -47,14 +47,14 @@ class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_det
         val ref = database.database.getReferenceFromUrl(FIRE_BASE_URL)
         // child 안에 무비 id 가져와야한다.
 
-        ref.child(args.movieCd.toString()).addValueEventListener(
+        ref.child("1").addValueEventListener(
             object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
                     if(dataSnapshot.value==null){//리뷰가 없을때
 
                     }
                     else{                        //리뷰가 있을때
-                        detailViewModel.searchReviewMovieList(args.movieCd!!.toInt())//리뷰 가져오기
+                        detailViewModel.searchReviewMovieList(1)//리뷰 가져오기
 
                     }
                 }
