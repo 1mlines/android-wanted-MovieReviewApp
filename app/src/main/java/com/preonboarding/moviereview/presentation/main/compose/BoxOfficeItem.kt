c4e752368261eaa3a84dbcf8dc8aa84a9d56664b
Hyunkuk <boris0815@naver.com>
2022-10-06 16:36:27 +0900

    feat : add box office item composable

---
+++ app/src/main/java/com/preonboarding/moviereview/presentation/main/compose/BoxOfficeItem.kt
@@ -0,0 +1,177 @@
+package com.preonboarding.moviereview.boxoffice.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import com.google.accompanist.placeholder.PlaceholderHighlight
+import com.google.accompanist.placeholder.material.placeholder
+import com.google.accompanist.placeholder.material.shimmer
+import com.preonboarding.moviereview.R
+import com.preonboarding.moviereview.domain.model.BoxOffice
+import timber.log.Timber
+
+/**
+ * @Created by 김현국 2022/10/05
+ */
+
+@Composable
+fun BoxOfficeItem(
+    modifier: Modifier = Modifier,
+    boxOffice: BoxOffice,
+    onClick: (BoxOffice) -> Unit
+) {
+    Card(
+        modifier = modifier.padding(start = 5.dp, end = 5.dp).shadow(
+            elevation = 3.dp,
+            shape = RoundedCornerShape(5.dp)
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .clickable {
+                    onClick(boxOffice)
+                },
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier.width(100.dp).fillMaxHeight()
+            ) {
+                if (boxOffice.moviePoster != "") {
+                    SubcomposeAsyncImage(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .width(100.dp)
+                            .clip(RoundedCornerShape(5.dp)),
+                        model = boxOffice.moviePoster,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop
+                    ) {
+                        val state = painter.state
+                        if (state is AsyncImagePainter.State.Loading) {
+                            Timber.tag("state").d("Loading")
+                            Box(
+                                modifier = Modifier.width(100.dp).fillMaxHeight().placeholder(
+                                    true,
+                                    highlight = PlaceholderHighlight.shimmer()
+                                )
+                            )
+                        } else if (state is AsyncImagePainter.State.Success) {
+                            Timber.tag("state").d("Success")
+                            SubcomposeAsyncImageContent()
+                        }
+                    }
+                }
+
+                Box(
+                    modifier = Modifier.fillMaxSize().align(Alignment.BottomCenter).background(
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                Color(0x00E4E8F5),
+                                Color(0xFF788098)
+                            )
+                        ),
+                        shape = RoundedCornerShape(5.dp)
+                    )
+                ) {
+                    Box(modifier = Modifier.padding(start = 3.dp).align(Alignment.BottomStart)) {
+                        Text(
+                            text = boxOffice.ranking,
+                            fontSize = 50.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    }
+                }
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(150.dp)
+            ) {
+                Text(
+                    modifier = Modifier,
+                    text = "영화 제목 : ${boxOffice.movieName}"
+                )
+                Text(
+                    modifier = Modifier,
+                    text = "개봉일 : ${boxOffice.movieOpen}"
+                )
+                Text(text = "관객 수 : ${boxOffice.audiAcc}")
+            }
+
+            if (boxOffice.rankInten.toInt() < 0) {
+                // 이미지 추가
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier.width(24.dp).height(24.dp),
+                        painter = painterResource(id = R.drawable.ic_down),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(1.dp))
+                    Text(text = boxOffice.rankInten)
+                }
+            } else if (boxOffice.rankInten.toInt() == 0) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier.width(24.dp).height(24.dp),
+                        painter = painterResource(id = R.drawable.ic_equal),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(1.dp))
+                }
+            } else {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier.width(24.dp).height(24.dp),
+                        painter = painterResource(id = R.drawable.ic_up),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(1.dp))
+                    Text(text = boxOffice.rankInten)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewBoxOfficeItem() {
+    MaterialTheme {
+        BoxOfficeItem(
+            boxOffice = BoxOffice.DUMMY
+        ) {
+        }
+    }
+}
