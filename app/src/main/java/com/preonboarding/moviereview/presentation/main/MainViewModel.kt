184ae5eb23577cb44bd15c1d3eae527c509e7ab4
Hyunkuk <boris0815@naver.com>
2022-10-06 20:26:08 +0900

    fix: List 불러오는 로직 수정 (병렬 처리)

--- app/src/main/java/com/preonboarding/moviereview/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/preonboarding/moviereview/presentation/main/MainViewModel.kt
@@ -4,71 +4,71 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.preonboarding.moviereview.data.mapper.asModel
 import com.preonboarding.moviereview.domain.model.BoxOffice
-import com.preonboarding.moviereview.domain.repository.RemoteRepository
 import com.preonboarding.moviereview.domain.usecase.DailyBoxOfficeUseCase
 import com.preonboarding.moviereview.domain.usecase.MovieInfosUseCase
 import com.preonboarding.moviereview.domain.usecase.PosterInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.zip
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val movieInfosUseCase: MovieInfosUseCase,
     private val dailyBoxOfficeUseCase: DailyBoxOfficeUseCase,
-    private val posterInfoUseCase: PosterInfoUseCase,
-    private val repository: RemoteRepository
+    private val posterInfoUseCase: PosterInfoUseCase
 ) : ViewModel() {
-    var movieCd = ""
-    fun getDailyBox(id: String, targetDt: String, wideAreaCd: String){
-        viewModelScope.launch {
-            val result = dailyBoxOfficeUseCase.getDailyBoxOfficeList(id, targetDt, wideAreaCd)
-            movieCd = result!!.boxOfficeResult!!.dailyBoxOfficeList[0].movieCd
-        }
+
+    private val _dailyMovieBoxList = MutableStateFlow(emptyList<BoxOffice>())
+    val dailyMovieBoxList = _dailyMovieBoxList.asStateFlow()
+
+    init {
+        val now = LocalDate.now()
+        val nowDate = now.minusDays(1)
+        // 날짜, 시간을 가져오고 싶은 형태 선언
+        val date = nowDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+        getBoxOfficeList(key = "d78dfcf081301ea1719d6d8d36756527", date = date)
     }
 
-    private fun test2() {
+    private fun getBoxOfficeList(key: String, date: String) {
         viewModelScope.launch {
-            _dailyMovieBoxList.value.forEachIndexed { index, item ->
-                repository.test2("d78dfcf081301ea1719d6d8d36756527", item.movieCd).collect { info ->
-                    val newList = repository.test3(info.movieInfoResult.movieInfo.movieNmEn, "fa8d1585").zip(
-                        _dailyMovieBoxList
-                    ) { poster, list ->
-                        if (poster.Poster != null) {
-                            list[index].copy(
-                                moviePoster = poster.Poster
-                            )
-                        }
+            val result = dailyBoxOfficeUseCase.getDailyBoxOfficeList(key, date)
+            if (result?.boxOfficeResult != null) {
+                _dailyMovieBoxList.value = result.boxOfficeResult.dailyBoxOfficeList.map {
+                    it.asModel()
+                }
 
-//                        list.map { boxOffice ->
-//                            if (poster.Poster != null) {
-//                                boxOffice.copy(
-//                                    moviePoster = poster.Poster
-//                                )
-//                            }
-//                        }
-                        list
-                    }
-                    newList.collect {
-                        _dailyMovieBoxList.value = it
+                _dailyMovieBoxList.value.forEachIndexed { index, boxOffice ->
+                    launch {
+                        val response = movieInfosUseCase.getMovieInfo(
+                            "d78dfcf081301ea1719d6d8d36756527",
+                            boxOffice.movieCd
+                        )
+                        if (response != null) {
+                            val poster = posterInfoUseCase.getPosterInfo(
+                                response.movieInfoResult.movieInfo.movieNmEn,
+                                key = "fa8d1585"
+                            )
+                            if (poster?.Poster != null) {
+                                _dailyMovieBoxList.value =
+                                    _dailyMovieBoxList.value.mapIndexed { mapIndex, boxOffice ->
+                                        if (mapIndex == index) {
+                                            boxOffice.copy(
+                                                moviePoster = poster.Poster
+                                            )
+                                        } else {
+                                            boxOffice
+                                        }
+                                    }
+                            }
+                        }
                     }
                 }
             }
         }
     }
-//    fun getDailyBox(id: String, targetDt: String) {
-//        viewModelScope.launch {
-//            val result = dailyBoxOfficeUseCase.getDailyBoxOfficeList(id, targetDt)
-//            movieCd = result!!.boxOfficeResult!!.dailyBoxOfficeList[0].movieCd
-//        }
-//    }
-//
-//    fun getPosterInfo(title: String, key: String) {
-//        viewModelScope.launch {
-//            val result = posterInfoUseCase.getPosterInfo(title, key)
-//        }
-//    }
 }
