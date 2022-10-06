47ab2abcc14f5dfee045a3782247e5b880fb5c36
Hyunkuk <boris0815@naver.com>
2022-10-06 23:15:38 +0900

    fix : 로직 수정

--- app/src/main/java/com/preonboarding/moviereview/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/preonboarding/moviereview/presentation/main/MainViewModel.kt
@@ -2,11 +2,11 @@ package com.preonboarding.moviereview.presentation.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.preonboarding.moviereview.presentation.main.mapper.asModel
 import com.preonboarding.moviereview.domain.model.BoxOffice
 import com.preonboarding.moviereview.domain.usecase.DailyBoxOfficeUseCase
 import com.preonboarding.moviereview.domain.usecase.MovieInfosUseCase
 import com.preonboarding.moviereview.domain.usecase.PosterInfoUseCase
+import com.preonboarding.moviereview.presentation.main.mapper.asModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -31,39 +31,37 @@ class MainViewModel @Inject constructor(
         val nowDate = now.minusDays(1)
         // 날짜, 시간을 가져오고 싶은 형태 선언
         val date = nowDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
-        getBoxOfficeList(key = "d78dfcf081301ea1719d6d8d36756527", date = date, code= "0105001")
+        getBoxOfficeList(key = "d78dfcf081301ea1719d6d8d36756527", date = date, code = "0105001")
     }
 
-    private fun getBoxOfficeList(key: String, date: String , code : String) {
+    private fun getBoxOfficeList(key: String, date: String, code: String) {
         viewModelScope.launch {
             val result = dailyBoxOfficeUseCase.getDailyBoxOfficeList(key, date, code)
             if (result?.boxOfficeResult != null) {
                 _dailyMovieBoxList.value = result.boxOfficeResult.dailyBoxOfficeList.map {
                     it.asModel()
                 }
-
-                _dailyMovieBoxList.value.forEachIndexed { index, boxOffice ->
+                result.boxOfficeResult.dailyBoxOfficeList.forEachIndexed { index, data ->
                     launch {
                         val response = movieInfosUseCase.getMovieInfo(
-                            "d78dfcf081301ea1719d6d8d36756527",
-                            boxOffice.movieCd
+                            key = "d78dfcf081301ea1719d6d8d36756527",
+                            movieCd = data.movieCd
                         )
                         if (response != null) {
                             val poster = posterInfoUseCase.getPosterInfo(
                                 response.movieInfoResult.movieInfo.movieNmEn,
                                 key = "fa8d1585"
                             )
-                            if (poster?.Poster != null) {
-                                _dailyMovieBoxList.value =
-                                    _dailyMovieBoxList.value.mapIndexed { mapIndex, boxOffice ->
-                                        if (mapIndex == index) {
-                                            boxOffice.copy(
-                                                moviePoster = poster.Poster
-                                            )
-                                        } else {
-                                            boxOffice
-                                        }
-                                    }
+
+                            _dailyMovieBoxList.value = _dailyMovieBoxList.value.map { boxOffice ->
+                                if (boxOffice.movieCd == data.movieCd) {
+                                    boxOffice.copy(
+                                        moviePoster = poster?.Poster ?: "",
+                                        isReady = true
+                                    )
+                                } else {
+                                    boxOffice
+                                }
                             }
                         }
                     }
