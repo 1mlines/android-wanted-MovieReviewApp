a456b1762740ba306606514b4cdd208ae89b6ce9
Hyunkuk <boris0815@naver.com>
2022-10-06 18:12:23 +0900

    feat : domain layer model 생성

---
+++ app/src/main/java/com/preonboarding/moviereview/domain/model/BoxOffice.kt
@@ -0,0 +1,28 @@
+package com.preonboarding.moviereview.domain.model
+
+/**
+ * @Created by 김현국 2022/10/05
+ */
+data class BoxOffice(
+    val movieCd: String,
+    val ranking: String,
+    val rankInten: String,
+    val rankType: String,
+    val movieName: String,
+    val audiAcc: String,
+    val moviePoster: String,
+    val movieOpen: String
+) {
+    companion object {
+        val DUMMY = BoxOffice(
+            movieCd = "",
+            ranking = "",
+            rankInten = "",
+            rankType = "OLD",
+            movieName = "DUMMY",
+            audiAcc = "",
+            moviePoster = "",
+            movieOpen = "2022/02/02"
+        )
+    }
+}
