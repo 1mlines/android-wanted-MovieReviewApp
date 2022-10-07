fa2ef4241d3fd2ed09830271d42b5b3334ce6568
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-10-07 21:25:33 +0900

    [DEL] Example Model 삭제

--- data/src/main/java/com/preonboarding/data/remote/response/boxoffice/BoxOfficeListResponse.kt
+++ data/src/main/java/com/preonboarding/data/remote/response/boxoffice/BoxOfficeListResponse.kt
@@ -1,11 +1,8 @@
 package com.preonboarding.data.remote.response.boxoffice
 
-import com.preonboarding.domain.model.ExampleModel
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BoxOfficeListResponse(
     val boxOfficeResult: BoxOfficeResult,
-) {
-    fun toDomainModel() = ExampleModel(null)
-}
+)
--- data/src/main/java/com/preonboarding/data/remote/response/movieinfo/MovieInfoResponse.kt
+++ data/src/main/java/com/preonboarding/data/remote/response/movieinfo/MovieInfoResponse.kt
@@ -1,11 +1,8 @@
 package com.preonboarding.data.remote.response.movieinfo
 
-import com.preonboarding.domain.model.ExampleModel
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MovieInfoResponse(
     val movieInfoResult: MovieInfoResult
-){
-    fun toDomainModel() = ExampleModel(null)
-}
+)
